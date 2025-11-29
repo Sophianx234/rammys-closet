3b6f1c2633142563e51cbd48ce9e4acda44932ff
sophianx234 <dx4336969@gmail.com>
2025-11-29 02:10:05 +0000

    feat: set priveleges on sidbar access @admin

--- src/components/featured-products.tsx
+++ src/components/featured-products.tsx
@@ -90,7 +90,7 @@ export default function FeaturedProducts() {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {products.map((product) => (
-            <ProductCard product={product}/>
+            <ProductCard key={product._id} product={product}/>
           ))}
         </div>
 
